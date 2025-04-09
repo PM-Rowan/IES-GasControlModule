7f87720f11c7688b20803e559f0ae7566a196b31
PM-Rowan <murphy136@students.rowan.edu>
2025-04-09 10:10:11 -0400

    removed main function operations

---
+++ RGB_LED.c
@@ -0,0 +1,36 @@
+#include "msp430fr2355.h"
+#include <msp430.h>
+#include <type_traits>
+
+void RGB_init()
+{
+    P6DIR |= BIT0 | BIT1 | BIT2;                     // P6.0, P6.1, and P6.2 output
+    P6SEL0 |= BIT0 | BIT1 | BIT2;                    // P6.0, P6.1, and P6.2 options select
+
+    TB3CCR0 = 1000 - 1;                         // PWM Period
+
+    // CCTL# Mode: reset/set
+    // CCR# Default to Off (0)
+
+    TB3CCTL1 = OUTMOD_3;
+    TB3CCR1 = 0;
+
+    TB3CCTL2 = OUTMOD_3;
+    TB3CCR2 = 0;
+    
+    TB3CCTL3 = OUTMOD_3;
+    TB3CCR3 = 0;
+
+    TB3CTL = TBSSEL__SMCLK | MC__UP | TBCLR;
+
+    return;
+}
+
+// char is an 8b/1B that can have values of (0-255)
+void RGB_PWM(char R, char G, char B)
+{
+    TB3CCR1 = R << 2; // R * 4: (0,255) -> (0,1020)
+    TB3CCR2 = G << 2; // G * 4: (0,255) -> (0,1020)
+    TB3CCR3 = B << 2; // B * 4: (0,255) -> (0,1020)
+    return;
+}
---
+++ RGB_LED.h
@@ -0,0 +1,7 @@
+#ifndef RGB_LED
+#define RGB_LED
+
+void RGB_init();
+void RGB_PWM(char, char, char);
+
+#endif
